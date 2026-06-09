9c869060e4efe7f5c11907827bc0c4d907d11455
Chandan kumar parida <chandan.parida@capsitech.com>
2026-06-09 11:54:03 +0530

    .

--- hello.js
+++ hello.js
@@ -6,9 +6,5 @@ console.log("I am chandan pro");
 console.log("I am bodhesh noob")
 
 console.log("Hi bro");
-<<<<<<< HEAD
-console.log("Welcome to capsitech")
-=======
 console.log("Welcome to capsitech")
 console.log("Welcome");
->>>>>>> 666949331340ea4c8d185f07e31f99fd6d26e4b3
